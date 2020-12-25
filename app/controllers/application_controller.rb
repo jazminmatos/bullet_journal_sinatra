13a71aefd79fa4e4176e37f7ea9cb707e1418068
jazminmatos <jmatos1993@gmail.com>
2020-12-24 21:54:02 -0500

    added view files & helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,24 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "shhh_its_a_secret"
   end
 
+  #loads the homepage
   get "/" do
     erb :index
   end
 
+  helpers do
+    #finds the current_user
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    #checks the boolean value of session to confirm whether or not a user is logged in
+    def logged_in?
+      !!session[:user_id]
+    end
+  end
 end
