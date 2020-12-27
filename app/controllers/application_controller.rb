b773e62069f35144b6102749193b52c21174b15e
jazminmatos <jmatos1993@gmail.com>
2020-12-27 00:17:39 -0500

    created post '/entries'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -109,9 +109,15 @@ class ApplicationController < Sinatra::Base
     end
   end
 
+  #receives user input from create from
+  #params => {"date" => "", "goal" => "", "log" => "", "gratitude" => "",}
+    ###Date = shouldn't accept if not a date (params doesn't accept it anyway)
+
   post '/entries' do
     if logged_in?
-      
+      binding.pry
+      @entry = Entry.new(params)
+      redirect '/entries/#{@entry.id}'
     else
       redirect '/login'
     end
